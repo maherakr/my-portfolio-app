76585f0926c23aeaab78196d5f0b373c9153bf34
Sistemee <dagedar123@gmail.com>
2025-04-22 03:44:27 -0400

    More projects added to the project section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,12 +2,33 @@ import React from "react";
 
 export default function Projects() {
 
-    const projectList = [{
+    const projectList = [
+        {
         title: "CCEAC Quiz maker",
-        describtion: "The online quiz platform can be used to quiz writers to write new quizzes, and to generate quizzes from preexisting quizzes and questions in a database.",
+        describtion: "The online quiz platform can be used to quiz writers to write new quizzes, and to generate quizzes from preexisting quizzes and questions in a database",
         image: "",
         link: "https://cceacquiz.azurewebsites.net/"
-    }]
+    },
+    {
+        title: "Planetary Position Tracker",
+        describtion: "Interactive app to visualize daily planetary positions over time using real orbital data",
+        image: "",
+        link: "https://maherakr-shiny.shinyapps.io/PlanetaryPositionTracker/"
+    },
+    {
+        title: "Expiry Date Tracker App",
+        describtion: "A shared product tracking app that uses barcode scanning and UPC APIs. Users can add items to a group dashboard and get notified before they expire.",
+        image: "",
+        link: ""
+    },
+    {
+        title: "Portfolio Website",
+        describtion: "My personal portfolio site — Where I showcase my projects, pretend to be organized and beg for job offers. Built to look better than my sleep schadual and where bugs occasionally evolve into features",
+        image: "",
+        link: ""
+    },
+
+]
 
     return (
         <section id="experience" className="py-16 px-4 max-w-5xl mx-auto">
