e1e2b8eb313a62913aa4e86df6c6fc0d04a69bf5
Sistemee <dagedar123@gmail.com>
2025-05-09 10:39:49 -0400

    Tools icons are added to projects cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,35 +1,120 @@
 import React from "react";
 import ImageModal from "./imageModal";
+import { title } from "process";
 
 export default function Projects() {
+    const toolsList = [
+        //0
+        {
+            title: "C#",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg"
+        },
+        //1
+        {
+            title: ".NET",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dot-net/dot-net-original.svg"
+        },
+        //2
+        {
+            title: "R",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/r/r-original.svg"
+        },
+        //3
+        {
+            title: "C++",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"
+        },
+        //4
+        {
+            title: "Java",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"
+        },
+        //5
+        {
+            title: "Node.js",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg"
+        },
+        //6
+        {
+            title: "Amazon RDS (SQL)",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original.svg"
+        },
+        //7
+        {
+            title: "Azure SQL",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azure/azure-original.svg"
+        },
+        //8
+        {
+            title: "Android Studio",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/androidstudio/androidstudio-original.svg"
+        },
+        //9
+        {
+            title: "JavaScript",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+        },
+        //10
+        {
+            title: "TypeScript",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
+        },
+        //11
+        {
+            title: "Next.js",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg"
+        },
+        //12
+        {
+            title: "React",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+        },
+        //13
+        {
+            title: "Tailwind CSS",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-plain.svg"
+        },
+        //14
+        {
+            title: "Git",
+            avatar: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
+        }
+    ];
 
     const projectList = [
         {
             title: "CCEAC Quiz maker",
             describtion: "The online quiz platform can be used to quiz writers to write new quizzes, and to generate quizzes from preexisting quizzes and questions in a database",
             image: "/images/cceacWebsite.jpg",
-            link: "https://cceacquiz.azurewebsites.net/"
+            link: "https://cceacquiz.azurewebsites.net/",
+            tools: [toolsList[0], toolsList[1], toolsList[7], toolsList[14]],
+
         },
         {
             title: "Planetary Position Tracker",
             describtion: "Interactive app to visualize daily planetary positions over time using real orbital data",
             image: "/images/planetaryPositionTrackerSite.jpg",
-            link: "https://maherakr-shiny.shinyapps.io/PlanetaryPositionTracker/"
+            link: "https://maherakr-shiny.shinyapps.io/PlanetaryPositionTracker/",
+            tools: [toolsList[2], toolsList[3], toolsList[14]],
         },
         {
             title: "Expiry Date Tracker App",
             describtion: "A shared product tracking app that uses barcode scanning and UPC APIs. Users can add items to a group dashboard and get notified before they expire.",
             image: "/images/ExpiryDateTrackerAppScreenshot.jpg",
-            link: ""
+            link: "",
+            tools: [toolsList[9], toolsList[5], toolsList[4], toolsList[8], toolsList[6], toolsList[14]]
         },
         {
             title: "Portfolio Website",
             describtion: "My personal portfolio site — Where I showcase my projects, pretend to be organized and beg for job offers. Built to look better than my sleep schadual and where bugs occasionally evolve into features",
             image: "/images/planetaryPositionTrackerSite.jpg",
-            link: ""
+            link: "",
+            tools: [toolsList[10], toolsList[11], toolsList[12], toolsList[13], toolsList[14]]
         },
 
     ]
+
+
 
     return (
         <section id="projects" className="py-16 px-4 max-w-5xl mx-auto">
@@ -52,7 +137,12 @@ export default function Projects() {
                                 <h2 className="card-title">{project.title}</h2>
                                 <p>{project.describtion}</p>
                                 <div className="card-actions justify-end">
-                                    <a
+                                    {index === 2 ? (
+                                        <button className="btn">
+                                            <span className="loading loading-spinner"></span>
+                                            In progress
+                                        </button>
+                                    ) : <a
                                         href={project.link}
                                         target="_blank"
                                         rel="noopener noreferrer"
@@ -60,6 +150,22 @@ export default function Projects() {
                                     >
                                         View Project
                                     </a>
+                                    }
+
+
+                                </div>
+                                <div className="card-actions justify-end">
+                                    
+                                    <div className="avatar-group -space-x-6">
+                                        {project.tools.map((tool, toolIndex) =>(
+                                        <div key={toolIndex} className="avatar">
+                                            <div className="w-12 ">
+                                                <img src={tool.avatar} className="bg-white"/>
+                                            </div>
+                                        </div>
+                                        ))}
+                                    </div>
+                                    
                                 </div>
                             </div>
                         </div>
